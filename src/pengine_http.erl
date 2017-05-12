c6cd636bb7f42ffd51fc1f9446c60c5854e24bf0
Limmen <kimham@kth.se>
2017-05-12 20:32:11 +0200

    fix malformed docs

--- src/pengine_http.erl
+++ src/pengine_http.erl
@@ -20,22 +20,22 @@ ping(Interval) ->
     ok.
 
 %% @doc
-%% Process the reply to a `pull_response`.  If the last answer was
+%% Process the reply to a pull_response.  If the last answer was
 %% final, this question will be asked to a death pengine.  We do not
 %% consider this an error.
 pull_respons() ->
     ok.
 
 %% @doc
-%% Invoke `/pengine/send`.  This method takes three parameters: the
-%% reply format (one of `prolog` or `json`), the pengine id (a UUID)
+%% Invoke /pengine/send.  This method takes three parameters: the
+%% reply format (one of prolog or json), the pengine id (a UUID)
 %% and the event (a serialized Prolog term).  In old versions, this
 %% was sent as a GET request.  This is undesirable, both because GET
 %% assumes a side-effect-free operation and because of the size limit
 %% to URLs imposed by some infrastructure.  The current version passes
 %% the format and id as URL parameters and the content as a POST body.
 %% Future versions might use the HTTP Accept header intead of format
-%% and add the id to the URL, i.e., using `/pengine/send/ID`
+%% and add the id to the URL, i.e., using /pengine/send/ID
 send(Event) ->
     ok.
 
