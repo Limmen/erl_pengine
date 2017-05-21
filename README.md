df7558c231b375d97509e37265ca73260ac28785
limmen <kimham@kth.se>
2017-05-21 11:51:18 +0200

    update tests

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ $ ./rebar3 compile
 ## Usage
 
 ```erlang
-{ok, P1, Id1} = pengine_master:create_pengine("http://127.0.0.1:4000/pengine", test_callbackmod, #{}).
+{ok, {P1, Id1}} = pengine_master:create_pengine("http://127.0.0.1:4000/pengine", test_callbackmod, #{}).
 
-{ok, P2, Id2} = pengine_master:create_pengine("http://127.0.0.1:4000/pengine", test_callbackmod, #{}).
+{ok, {P2, Id2}} = pengine_master:create_pengine("http://127.0.0.1:4000/pengine", test_callbackmod, #{}).
 
 Pengines = pengine_master:list_pengines().
 
@@ -43,6 +43,25 @@ pengine:next(P1).
 {ok,<<"{\"event\":\"destroy\", \"id\":\"38612376351347808823784683757103067622\"}">>}
 7>
 
+{ok, StatusCode, Headers, ClientRef} = hackney:get(<<"http://127.0.0.1:4000/pengine/ping">>, [{<<"Content-Type">>, <<"application/json; charset=utf-8">>}, {<<"Accept">>, <<"application/json">>}], jsx:encode(#{id => Id1, format => "json"}), []).
+
+{ok, StatusCode, Headers, ClientRef} = hackney:post(<<"http://127.0.0.1:4000/pengine/create">>, [{<<"Content-Type">>, <<"application/json; charset=utf-8">>}, {<<"Accept">>, <<"application/json">>}], jsx:encode(#{application => "pengine_sandbox", chunk => 1, destroy => true, format => json}), []).
+
+
+7> {ok, StatusCode1, Headers1, ClientRef1} = hackney:get(<<"http://127.0.0.1:4000/pengine/ping?id=27aacaba-42fd-4eb6-9157-ae5229cc7ae4">>, [{<<"Content-Type">>, <<"application/json; charset=utf-8">>}, {<<"Accept">>, <<"application/json">>}], <<>>, []).
+{ok,200,
+    [{<<"Date">>,<<"Sun, 21 May 2017 09:50:04 GMT">>},
+     {<<"Content-Type">>,<<"text/x-prolog; charset=UTF-8">>},
+     {<<"Connection">>,<<"Keep-Alive">>},
+     {<<"Content-Length">>,<<"428">>}],
+    #Ref<0.0.1.643>}
+8> {ok, Body} = hackney:body(ClientRef1).
+{ok,<<"ping('27aacaba-42fd-4eb6-9157-ae5229cc7ae4',thread{id:12,stacks:stacks{global:stack{allocated:61424,limit:26"...>>}
+9> Body.
+<<"ping('27aacaba-42fd-4eb6-9157-ae5229cc7ae4',thread{id:12,stacks:stacks{global:stack{allocated:61424,limit:268435456,"...>>
+10> io:format("~p", [Body]).
+<<"ping('27aacaba-42fd-4eb6-9157-ae5229cc7ae4',thread{id:12,stacks:stacks{global:stack{allocated:61424,limit:268435456,name:global,usage:2224},local:stack{allocated:28672,limit:268435456,name:local,usage:1408},total:stack{allocated:120808,limit:805306368,name:stacks,usage:4296},trail:stack{allocated:30712,limit:268435456,name:trail,usage:664}},status:running,time:time{cpu:0.023156854,epoch:1495360028.0125349,inferences:199}}).\n">>ok
+
 ```
 
 ```bash
@@ -51,6 +70,24 @@ curl --data "event=destroy.\n" -H "Content-Type: application/x-prolog" "http://l
 
 curl --data "event=destroy.\n" -H "Content-Type: application/x-prolog" "http://localhost:4000/pengine/send?format=json&id=26520944056946901811031529588524459285"
 
+curl --data "id=&format=json" -H "Content-Type: application/json" "http://localhost:4000/pengine/ping"
+
+curl http://localhost:4000/pengine/ping -d '{"id" : , "format": "json"}'
+
+curl http://localhost:4000/pengine/create
+
+
+curl http://localhost:4000/pengine/create
+
+
+kim@limmen ~> curl "http://localhost:4000/pengine/ping?id=2ab11b38-21fb-4a8b-bc2d-ca78bcc78f4f"
+ping('2ab11b38-21fb-4a8b-bc2d-ca78bcc78f4f',thread{id:12,stacks:stacks{global:stack{allocated:61424,limit:268435456,name:global,usage:2184},local:stack{allocated:28672,limit:268435456,name:local,usage:1408},total:stack{allocated:120808,limit:805306368,name:stacks,usage:4256},trail:stack{allocated:30712,limit:268435456,name:trail,usage:664}},status:running,time:time{cpu:0.037169991,epoch:1495358455.875498,inferences:197}}).
+kim@limmen ~> curl "http://localhost:4000/pengine/ping?id=2ab11b38-21fb-4a8b-bc2d-ca78bcc78f"
+died('2ab11b38-21fb-4a8b-bc2d-ca78bcc78f').
+kim@limmen ~> curl "http://localhost:4000/pengine/ping?id=2ab11b38-21fb-4a8b-bc2d-ca78bcc78f4f"
+ping('2ab11b38-21fb-4a8b-bc2d-ca78bcc78f4f',thread{id:12,stacks:stacks{global:stack{allocated:61424,limit:268435456,name:global,usage:2184},local:stack{allocated:28672,limit:268435456,name:local,usage:1408},total:stack{allocated:120808,limit:805306368,name:stacks,usage:4256},trail:stack{allocated:30712,limit:268435456,name:trail,usage:664}},status:running,time:time{cpu:0.038939034,epoch:1495358455.875498,inferences:197}}).
+
+curl -H "Accept: application/json" -H "Content-Type: application/json" "http://localhost:4000/pengine/ping?id=f04494dc-2ef7-4dd5-96e2-dfcb33dcdfcc"
 
 ```
 
--- test/pengine_pltp_http_tests.erl
+++ test/pengine_pltp_http_tests.erl
@@ -18,19 +18,129 @@
 %% TESTS
 %%===================================================================
 
-%% Test generator for module pengine_pltp_http
--spec pengine_pltp_http_test_() -> list().
-pengine_pltp_http_test_() ->
-    %% add your asserts in the returned list, e.g.:
-    %% [
-    %%   ?assert(?MODNAME:double(2) =:= 4),
-    %%   ?assertMatch({ok, Pid}, ?MODNAME:spawn_link()),
-    %%   ?assertEqual("ba", ?MODNAME:reverse("ab")),
-    %%   ?assertError(badarith, ?MODNAME:divide(X, 0)),
-    %%   ?assertExit(normal, ?MODNAME:exit(normal)),
-    %%   ?assertThrow({not_found, _}, ?MODNAME:func(unknown_object))
-    %% ]
-    [].
+%% tests create
+-spec create_test_() -> tuple().
+create_test_()->
+    {"create test", {foreach,
+                     fun() ->
+                             meck:new(hackney)
+                     end,
+                     fun(_) ->
+                             meck:unload(hackney)
+                     end,
+                     [
+                      fun()->
+                              meck:expect(hackney, post, fun(_, _, _, _) -> 
+                                                                 {ok, 200, [], ref1} 
+                                                         end),
+                              Res = <<"{\n  \"event\":\"create\",\n  \"id\":\"e8d94208-168e-42c2-8666-f938c9a5f388\",\n  \"slave_limit\":3\n}">>,
+                              meck:expect(hackney, body, fun(ref1) -> 
+                                                                 {ok,
+                                                                  Res
+                                                                 } end),
+                              ?assertMatch({ok, #{
+                                              <<"event">> := <<"create">>,
+                                              <<"id">> := <<"e8d94208-168e-42c2-8666-f938c9a5f388">>,
+                                              <<"slave_limit">> := 3
+                                             }},
+                                           pengine_pltp_http:create("127.0.0.1/pengine", #{
+                                                                      application => "pengine_sandbox",
+                                                                      chunk => 1, 
+                                                                      destroy => true, 
+                                                                      format => json
+                                                                     }))
+                      end
+                     ]
+                  }
+    }.
+
+%% tests ping
+-spec ping_test_() -> tuple().
+ping_test_()->
+    {"ping test", {foreach,
+                   fun() ->
+                           meck:new(hackney)
+                   end,
+                   fun(_) ->
+                           meck:unload(hackney)
+                   end,
+                   [
+                    fun()->
+                            ok
+                    end
+                   ]
+                         }
+    }.
+
+%% tests abort
+-spec abort_test_() -> tuple().
+abort_test_()->
+    {"abort test", {foreach,
+                    fun() ->
+                            meck:new(hackney)
+                    end,
+                    fun(_) ->
+                            meck:unload(hackney)
+                    end,
+                    [
+                     fun()->
+                             ok
+                     end
+                    ]
+                  }
+    }.
+
+%% tests pull_response
+-spec pull_response_test_() -> tuple().
+pull_response_test_()->
+    {"pull_response test", {foreach,
+                            fun() ->
+                                    meck:new(hackney)
+                            end,
+                            fun(_) ->
+                                    meck:unload(hackney)
+                            end,
+                            [
+                             fun()->
+                                     ok
+                             end
+                            ]
+                  }
+    }.
+
+%% tests ping
+-spec send_test_() -> tuple().
+send_test_()->
+    {"send test", {foreach,
+                   fun() ->
+                           meck:new(hackney)
+                   end,
+                   fun(_) ->
+                           meck:unload(hackney)
+                   end,
+                   [
+                    fun()->
+                            meck:expect(hackney, post, fun(_, _, _, _) -> 
+                                                               {ok, 200, [], ref1} 
+                                                       end),
+                            Res = <<"{\"event\":\"destroy\", \"id\":\"38612376351347808823784683757103067622\"}">>,
+                            meck:expect(hackney, body, fun(ref1) -> 
+                                                               {ok,
+                                                                Res
+                                                               } end),
+                            Id = <<"38612376351347808823784683757103067622">>,
+                            ?assertMatch({ok, 
+                                          #{
+                                            <<"event">> := <<"destroy">>,
+                                            <<"id">> := Id
+                                           }},
+                                         pengine_pltp_http:send(Id, "127.0.0.1/pengine", "destroy", "json"))
+                    end
+                   ]
+                  }
+    }.
+
+
 
 
 %%===================================================================
