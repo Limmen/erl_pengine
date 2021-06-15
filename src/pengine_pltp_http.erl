6e06b2f7cb985df9f228115b61ec4c15b167c6a4
Kim Hammar <kimham@kth.se>
2021-06-15 17:26:11 +0200

    test

--- src/pengine_pltp_http.erl
+++ src/pengine_pltp_http.erl
@@ -77,6 +77,9 @@ send(Id, Server, Event, Format) ->
 create(Server, Options) ->
     Options1 = options_to_binary(Options),
     URL = list_to_binary(Server ++ "/create"),
+    io:format("Create test \n"),
+    io:format("URL: ~s\n", [URL]),
+    io:format("URL: ~s\n", [Options1]),
     case hackney:post(URL, [json_content_type(), json_accept_header()], options_to_json(Options1), []) of
         {ok, _StatusCode, _Headers, ClientRef} ->
             {ok, Body} = hackney:body(ClientRef),
